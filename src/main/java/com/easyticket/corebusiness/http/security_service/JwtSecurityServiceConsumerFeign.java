ce9b5cf4c345447a4a681274b2556f4091d1af68
RonyLiboni <ronald.liboni@acad.pucrs.br>
2022-11-08 13:47:19 -0300

    FIX corrects the API call in the Feign to security-ms

--- src/main/java/com/easyticket/corebusiness/http/security_service/JwtSecurityServiceConsumerFeign.java
+++ src/main/java/com/easyticket/corebusiness/http/security_service/JwtSecurityServiceConsumerFeign.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RequestHeader;
 @FeignClient(value = "jwt-security-consumer", url = "http://localhost:8080/security-ms")
 public interface JwtSecurityServiceConsumerFeign {
 	
-	@PostMapping("/v1/user")
+	@PostMapping("/all/v1/users")
 	public ResponseEntity<?> registerNewUser(NewUserRequest request); 
 	
-	@DeleteMapping("/admin/v1/user/{username}")
+	@DeleteMapping("/admin/v1/users/{username}")
 	public ResponseEntity<Void> deleteUserByUsername(@RequestHeader(value = "Authorization", required = true) String authorizationHeader, 
 																					@PathVariable("username") String username);
 		
